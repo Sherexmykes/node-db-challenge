fb7bddeb2efd93a2dee1abaa92ee5be9a76db9b0
Sheila R Moore <sherexmykes@github.com>
2020-03-06 22:10:20 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const helmet = require("helmet")
+const projectRouter = require("./routers/projectRouter");
+const resourcesRouter = require("./routers/resourcesRouter");
 
 const server = express();
 
 
 server.use(express.json());
 server.use(helmet());
-
+server.use("/api/projects", projectRouter);
+server.use("/api/resources", resourcesRouter);
 
 module.exports = server;
